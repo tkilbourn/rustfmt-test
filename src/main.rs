08a4cc21f02c394f35451574da8ee8513b031621
Tim Kilbourn <tkilbourn@google.com>
2018-08-03 16:34:29 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod other;
+
+fn main() {
+    other::long_function(0, 1, 1.5, String::new(), 2, &[0]);
+}
---
+++ src/other.rs
@@ -0,0 +1,9 @@
+pub (crate) fn long_function(
+    that: u64, does: u32,
+    not: f32, follow: String, the: usize,
+    rules: &[u8]) ->
+Result<(), isize> {
+    println!("hello, world!"
+             );
+    Ok(())
+}
